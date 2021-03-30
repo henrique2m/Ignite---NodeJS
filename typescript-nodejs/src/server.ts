108e1f84f3f84c560cadc89904b137b425f134b4
henrique2m <btdavintehenrique@gmail.com>
2021-03-29 21:18:55 -0300

    chore: :sparkles: add types

--- typescript-nodejs/src/server.ts
+++ typescript-nodejs/src/server.ts
@@ -1,11 +1,10 @@
 import express from 'express';
+import { CreateCourse } from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (request, response) => {
-  return response.send("Hello World!");
-});
+app.get('/',  CreateCourse);
 
 app.listen(3333, () => {console.log('Staring server!')})
