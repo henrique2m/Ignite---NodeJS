c2e192337e8b0ff3b0b280dce2ab69f91b8c947e
henrique2m <btdavintehenrique@gmail.com>
2021-05-04 23:53:33 -0300

    chore: :sparkles: create specification migration :rocket:

--- rentalx/src/shared/container/index.ts
+++ rentalx/src/shared/container/index.ts
@@ -2,8 +2,15 @@ import { container } from "tsyringe";
 
 import { ICategoriesRepository } from "../../modules/cars/repositories/ICreateCategoriesRepository";
 import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository";
+import { SpecificationsRepository } from "../../modules/cars/repositories/implementations/SpacificationsRepository";
+import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
 
 container.registerSingleton<ICategoriesRepository>(
   "CategoryRepository",
   CategoriesRepository
+);
+
+container.registerSingleton<ISpecificationsRepository>(
+  "SpecificationsRepository",
+  SpecificationsRepository
 );
