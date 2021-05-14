0af97e4a02d3c8f83f0714d816d26aedd79b666b
henrique2m <btdavintehenrique@gmail.com>
2021-05-13 23:23:38 -0300

    chore: :sparkles: add jwt authentication :rocket:

--- rentalx/src/routes/index.ts
+++ rentalx/src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+import { authenticateRoutes } from "./authenticate.routes";
 import { categoriesRoutes } from "./categories.routes";
 import { specificationsRoutes } from "./specifications.routes";
 import { usersRouters } from "./users.routes";
@@ -11,5 +12,7 @@ router.use("/categories", categoriesRoutes);
 router.use("/specifications", specificationsRoutes);
 
 router.use("/users", usersRouters);
+
+router.use(authenticateRoutes);
 
 export { router };
