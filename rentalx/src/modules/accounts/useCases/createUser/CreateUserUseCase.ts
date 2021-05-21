b067534b9477dca4665551e84a523ce7d6ea24b0
henrique2m <btdavintehenrique@gmail.com>
2021-05-20 23:28:00 -0300

    chore: :sparkles: upload avatar :rocket:

--- rentalx/src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ rentalx/src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,9 +1,9 @@
 import { hash } from "bcryptjs";
 import { inject, injectable } from "tsyringe";
 
-import { AppError } from "../../../errors/AppError";
-import { ICreateUsersDTO } from "../dtos/ICreateUserDTO";
-import { IUsersRepository } from "../repositories/IUsersRepository";
+import { AppError } from "../../../../errors/AppError";
+import { ICreateUsersDTO } from "../../dtos/ICreateUserDTO";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 @injectable()
 class CreateUserUseCase {
