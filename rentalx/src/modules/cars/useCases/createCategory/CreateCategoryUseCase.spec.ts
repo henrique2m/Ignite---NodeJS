cba4dc43291fcd6d6c7bf991602dbbe4b9635641
henrique2m <btdavintehenrique@gmail.com>
2021-05-26 23:54:58 -0300

    test: :sparkles: add category creation test :rocket:

--- rentalx/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ rentalx/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -1,6 +1,52 @@
-describe("jest", () => {
-  it("started test", () => {
-    const test = "jest";
-    expect(test).toBe("jest");
+import { AppError } from "../../../../errors/AppError";
+import { CategoriesRepositoryInMemory } from "../../../accounts/repositories/in-memory/CategoriesRepositoryInMemory";
+import { CreateCategoryUserCase } from "./CreateCategoryUserCase";
+
+let createCategoryUseCase: CreateCategoryUserCase;
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
+
+describe("Create Category", () => {
+  beforeEach(() => {
+    categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+    createCategoryUseCase = new CreateCategoryUserCase(
+      categoriesRepositoryInMemory
+    );
+  });
+
+  it("should be able to create a new category", async () => {
+    const category = {
+      name: "Category Test",
+      description: "Category description Test",
+    };
+
+    await createCategoryUseCase.execute({
+      name: category.name,
+      description: category.description,
+    });
+
+    const categoryCreated = await categoriesRepositoryInMemory.findByName(
+      category.name
+    );
+
+    expect(categoryCreated).toHaveProperty("id");
+  });
+
+  it("should be able to create a new category with name exists", async () => {
+    expect(async () => {
+      const category = {
+        name: "Category Test",
+        description: "Category description Test",
+      };
+
+      await createCategoryUseCase.execute({
+        name: category.name,
+        description: category.description,
+      });
+
+      await createCategoryUseCase.execute({
+        name: category.name,
+        description: category.description,
+      });
+    }).rejects.toBeInstanceOf(AppError);
   });
 });
