98bf98cc8f842971d8d01ab1317656b335742b12
henrique2m <btdavintehenrique@gmail.com>
2021-07-05 21:44:06 -0300

    test: :sparkles: add user authentication test :rocket:

---
+++ rentalx/src/modules/accounts/repositories/in-memory/UsersRepositoryInMemory.ts
@@ -0,0 +1,35 @@
+import { ICreateUsersDTO } from "../../dtos/ICreateUserDTO";
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../IUsersRepository";
+
+class UsersRepositoryInMemory implements IUsersRepository {
+  users: User[] = [];
+
+  async create({
+    driver_license,
+    email,
+    name,
+    password,
+  }: ICreateUsersDTO): Promise<void> {
+    const user = new User();
+
+    Object.assign(user, {
+      driver_license,
+      email,
+      name,
+      password,
+    });
+
+    this.users.push(user);
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    return this.users.find((use) => use.email === email);
+  }
+
+  async findById(id: string): Promise<User> {
+    return this.users.find((use) => use.id === id);
+  }
+}
+
+export { UsersRepositoryInMemory };
